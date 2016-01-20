7f30d59c7b10501b948301f5717d10ad1fc26f13
Lance Zhu <lzhu@lai.io>
2016-01-20 00:51:19 -0800

    fix(Address): fix bugs caused by breaking changes in underlying library
    Summary: fix bug
    
    Test Plan: N/A
    
    Reviewers: cwang, wqi
    
    Differential Revision: https://dev.lai.io/D142

--- server/cloud/controller/address/Address.js
+++ server/cloud/controller/address/Address.js
@@ -1,7 +1,6 @@
 /**
  * Created by hxucaa on 2015-12-12.
  */
-/* eslint-disable prefer-template, newline-after-var */
 
 "use strict";
 
@@ -10,6 +9,41 @@ import { validation, model } from "leancloud-utility";
 
 const { AddressValidation } = validation;
 const { Region } = model;
+
+function processRegion(request, response) {
+
+  const address = request.object;
+  const street = address.get("street");
+  const regionCode = address.get("regionCode");
+
+  if (regionCode) {
+    const region = Region.matchByCode(regionCode);
+
+    if (Region.isMunicipality(region)) {
+      const province = Region.getParent(region);
+      const fullAddress = `${province.regionNameC}${region.regionNameC}${street}`;
+
+      address.set("fullAddress", fullAddress);
+      address.set("city", region.code);
+      address.set("province", province.code);
+    }
+    else if (Region.isDistrict(region)) {
+      const municipality = Region.getParent(region);
+      const province = Region.getParent(municipality);
+      const fullAddress = `${province.regionNameC}
+                          ${municipality.regionNameC}
+                          ${region.regionNameC}
+                          ${street}`;
+
+      address.set("fullAddress", fullAddress);
+      address.set("city", municipality.code);
+      address.set("province", province.code);
+    }
+    else {
+      response.error("Region is not level 2 or level 3");
+    }
+  }
+}
 
 export const beforeSave = function(request, response) {
 
@@ -25,29 +59,7 @@ export const beforeSave = function(request, response) {
    );
 
   // Operation
-  if (regionCode) {
-    const region = Region.matchByCode(regionCode);
-
-    if (Region.isMunicipality(region)) {
-      const province = Region.parent(region);
-      const fullAddress = province.regionNameC +
-                          region.regionNameC + street;
-
-      address.set("fullAddress", fullAddress);
-    }
-    else if (Region.isDistrict(region)) {
-      const municipality = Region.parent(region);
-      const province = Region.parent(municipality);
-      const fullAddress = province.regionNameC +
-                          municipality.regionNameC +
-                          region.regionNameC + street;
-
-      address.set("fullAddress", fullAddress);
-    }
-    else {
-      response.error("Region is not level 2 or level 3");
-    }
-  }
+  processRegion(request, response);
 
   response.success();
 };
@@ -66,29 +78,7 @@ export const beforeUpdate = function(request, response) {
    );
 
   // Operation
-  if (regionCode) {
-    const region = Region.matchByCode(regionCode);
-
-    if (Region.isMunicipality(region)) {
-      const province = Region.parent(region);
-      const fullAddress = province.regionNameC +
-                          region.regionNameC + street;
-
-      address.set("fullAddress", fullAddress);
-    }
-    else if (Region.isDistrict(region)) {
-      const municipality = Region.parent(region);
-      const province = Region.parent(municipality);
-      const fullAddress = province.regionNameC +
-                          municipality.regionNameC +
-                          region.regionNameC + street;
-
-      address.set("fullAddress", fullAddress);
-    }
-    else {
-      response.error("Region is not level 2 or level 3");
-    }
-  }
+  processRegion(request, response);
 
   response.success();
 };
